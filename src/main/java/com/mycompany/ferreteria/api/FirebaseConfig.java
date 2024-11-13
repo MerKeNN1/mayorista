f6b78b87d1f5d61559d304b2c90d39c48371d009
MerKeNN1 <om899661@gmail.com>
2024-11-12 22:11:36 -0600

    Use environment variables for Firebase configuration

--- src/main/java/com/mycompany/ferreteria/api/FirebaseConfig.java
+++ src/main/java/com/mycompany/ferreteria/api/FirebaseConfig.java
@@ -5,11 +5,13 @@ import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import java.io.ByteArrayInputStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
@@ -17,13 +19,17 @@ public class FirebaseConfig {
     @Bean
     public void firebaseInitialization() {
         try {
-            // Cargar el archivo de credenciales
-            FileInputStream serviceAccount = new FileInputStream("src/main/resources/Mayoristav2.json");
+            // Obtener las credenciales desde la variable de entorno 
+            String firebaseCredentials = System.getenv("FIREBASE_CREDENTIALS"); 
+            InputStream serviceAccount = new ByteArrayInputStream(firebaseCredentials.getBytes());
 
+            // Obtener la URL de la base de datos desde la variable de entorno 
+            String databaseUrl = System.getenv("FIREBASE_DATABASE_URL");
+            
             // Configurar las opciones de Firebase
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://mayorista-89074.firebaseio.com") // Asegúrate de colocar el nombre correcto de tu base de datos
+                    .setDatabaseUrl(databaseUrl) // Asegúrate de colocar el nombre correcto de tu base de datos
                     .build();
 
             // Inicializar la aplicación de Firebase
