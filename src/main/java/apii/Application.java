082c3abdfe51d4c5e950765f0b8755a66c5b492b
MerKeNN1 <om899661@gmail.com>
2024-11-16 11:24:09 -0600

    Exclude DataSource auto-configuration in Application class

--- src/main/java/apii/Application.java
+++ src/main/java/apii/Application.java
@@ -4,8 +4,9 @@ package apii;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Application {
 
     public static void main(String[] args) {
